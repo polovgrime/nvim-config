17db1e79e3e97badbefc34de450a5a2c08318487
polovgrime <polovgrime@gmail.com>
2023-07-29 17:52:44 +0300

    meh

---
+++ nvim/test/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+var i = 0;
+while (i++ < 25)
+    Console.Beep();
+Console.WriteLine("Hello, World!");
+var testValue = "test";
+Console.WriteLine(testValue);
+
--- test/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-var testValue = "test";
-Console.WriteLine(testValue);
-
